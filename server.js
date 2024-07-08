a5e3816944667b3ffc35b82a28e5f8ec63ad7f00
luckybutter1011 <luckybutter322982@gmail.com>
2024-07-09 05:48:30 +1000

    fix the cors

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ require("./libs/mongodb");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: "https://bitcoin-millionaire-html-5.vercel.app",
+  })
+);
 
 //api
 app.use(userinfoApi);
@@ -20,4 +24,3 @@ app.use(userinfoApi);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-  
